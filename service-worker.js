1f260a158c4cad7cb7e8e5f2852be2416a6a705e
ItsBrian <38297733+ItsBrian@users.noreply.github.com>
2022-09-10 05:38:33 +0200

    full game (no ios blobs)

--- service-worker.js
+++ service-worker.js
@@ -2,6 +2,7 @@
 const version = 7_9_2022;
 
 // Workbox
+self.__WB_DISABLE_DEV_LOGS = true
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
 
 workbox.routing.registerRoute(
